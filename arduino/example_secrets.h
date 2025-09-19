d28ae002dd379593a63ea0f861b6ca53d73244a9
dreisday <dreisday@gmail.com>
2025-09-19 18:26:12 +0100

    added example secrets.h

---
+++ arduino/example_secrets.h
@@ -0,0 +1,12 @@
+// secrets.h
+#pragma once
+
+// WiFi Settings
+#define WIFI_SSID       "WiFi_SSID"
+#define WIFI_PASSWORD   "WiFi_Password"
+
+// MQTT Settings
+#define MQTT_SERVER     "MQTT_Server_host_or_ip"
+#define MQTT_PORT       1883 // change if needed
+#define MQTT_USER       "MQTT_Username"
+#define MQTT_PASS       "MQTT_Password"
